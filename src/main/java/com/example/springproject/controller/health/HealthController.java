9958a029393e9a812009c90882f9ed7409a2a1f5
DanielOchoa <danochoa1412@gmail.com>
2023-08-26 18:08:09 -0500

    Swagger añadido

--- src/main/java/com/example/springproject/controller/health/HealthController.java
+++ src/main/java/com/example/springproject/controller/health/HealthController.java
@@ -1,12 +1,14 @@
 package com.example.springproject.controller.health;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/health")
 public class HealthController {
 
-    @RequestMapping("/health")
+    @GetMapping
     public String checkAPI() {
         return "<h1>The API is working ok!</h1>";
     }
